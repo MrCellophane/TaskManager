f1fe5aff5db1af4c0d8cec3c890e69acb54767a9
Nastya Kulakova <nvk@8xx8.ru>
2019-12-02 21:03:59 +0400

    Fixed State Machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,42 +1,37 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :assignee, class_name: 'User', optional: true
-    
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :author, presence: true
-    validates :description, length: { maximum: 500 }
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-    class Vehicle < ActiveRecord::Base
-        state_machine :state, initial => :new_task do
-          event :archived do
-            transition :new_task => archived
-            transition :released => archived
-          end
-          
-          event :released do
-            transition :ready_for_release => released
-          end
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
 
-          event :ready_for_release do
-            transition :in_code_review => ready_for_release
-          end
+  state_machine :state, initial => :new_task do
+    event :archived do
+      transition %i[new_task released] => :archived
+    end
 
-          event :in_code_review do
-            transition :in_qa => in_code_review
-          end
+    event :released do
+      transition ready_for_release: :released
+    end
 
-          event :in_qa do 
-            transition :in_development => in_qa
-          end
-          
-          event :in_development do
-            transition :in_qa => in_development
-            transition :in_code_review => in_development
-            transition :new_task => in_development
-          end
-        end
-      
+    event :start_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :start_code_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :start_qa do
+      transition in_development: :in_qa
+    end
+
+    event :start_development do
+      transition %i[in_qa in_code_review new_task] => :in_development
+    end
+  end
 end
