1bde0b1a326d1a0c5981197934132b342e95f59f
Nastya Kulakova <nvk@8xx8.ru>
2019-12-02 20:36:58 +0400

    Create State Machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,35 @@ class Task < ApplicationRecord
     validates :description, presence: true
     validates :author, presence: true
     validates :description, length: { maximum: 500 }
+
+    class Vehicle < ActiveRecord::Base
+        state_machine :state, initial => :new_task do
+          event :archived do
+            transition :new_task => archived
+            transition :released => archived
+          end
+          
+          event :released do
+            transition :ready_for_release => released
+          end
+
+          event :ready_for_release do
+            transition :in_code_review => ready_for_release
+          end
+
+          event :in_code_review do
+            transition :in_qa => in_code_review
+          end
+
+          event :in_qa do 
+            transition :in_development => in_qa
+          end
+          
+          event :in_development do
+            transition :in_qa => in_development
+            transition :in_code_review => in_development
+            transition :new_task => in_development
+          end
+        end
+      
 end
