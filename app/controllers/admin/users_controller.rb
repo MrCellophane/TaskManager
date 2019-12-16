e0ddf2b2fbcdfa6d8174700f1d9867d2ee26886d
Nastya Kulakova <nvk@8xx8.ru>
2019-12-16 23:12:44 +0400

    authenticate for board

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,18 +3,22 @@ class Admin::UsersController < Admin::ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def create
     @user = User.new(user_params)
-     if @user.save
+    if @user.save
       redirect_to admin_users_url
     else
       render :new_admin_user_url
@@ -29,11 +33,13 @@ class Admin::UsersController < Admin::ApplicationController
       render :edit
     end
   end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to admin_users_path
   end
+
   private
 
   def user_params
--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class Web::SessionsControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
-    get board_url
+    get new_session_url
     assert_response :success
   end
   test 'should post create' do
